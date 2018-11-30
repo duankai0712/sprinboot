7cc2baba7b3b52d9c9e58a98b7c2e1904f09ad01
“your_uduankai <1106734690@qq.com>
2018-11-30 22:34:15 +0800

    mysql 连接

--- src/main/java/com/znsd/springboot/controller/HeeloController.java
+++ src/main/java/com/znsd/springboot/controller/HeeloController.java
@@ -1,60 +1,64 @@
 package com.znsd.springboot.controller;
 
 
+import com.znsd.springboot.Service.StudentServiceImpl;
+import com.znsd.springboot.entitys.Grede;
 import com.znsd.springboot.entitys.Student;
 import com.znsd.springboot.entitys.StudentService;
 import com.znsd.springboot.error.MyException;
-import com.znsd.springboot.error.MyException_DJ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 
 /**
- * Created by Administrator on 2018/11/28.
- */
+* @Author duankai
+* @Date 2018/11/30  16:14
+*/
 @Controller
 public class HeeloController {
 
     @Autowired
     private Student studen;
     @Autowired
-    public StudentService studentService;
+    private StudentService studentService;
+    @Autowired
+    private StudentServiceImpl studentServiceImpl;
+    @Resource
+    private DataSource dataSource;
     @ResponseBody
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         System.out.println(studen);
         System.out.println(studentService);
         List list = new ArrayList();
-        return  studen.toString();
+        return studen.toString();
     }
 
     @RequestMapping("/student")
-    public String longing(){
+    public String longing() {
         System.out.println("该有");
         return "hello";
     }
 
     @RequestMapping("demo/{id}")
-    public String demo(@PathVariable("id")Integer id){
-        if(id==5){
-           throw new MyException();
+    public String demo(@PathVariable("id") Integer id, HttpServletRequest request)throws Exception {
+        if (id == 5) {
+            throw new MyException();
         }
+        List<Grede> list =  studentServiceImpl.findStudent();
+        request.setAttribute("grede",list);
+        //输出连接池是否连接成功
+        System.out.println(this.dataSource.getConnection());
         return "demo";
     }
 
-    //-----DJ test代码
-
-    @RequestMapping("DJ/{name}")
-    public String demo2(@PathVariable("name")String name){
-        if("狗子".equals(name)){
-            throw new MyException_DJ();
-        }
-        return "demo";
-    }
 }
