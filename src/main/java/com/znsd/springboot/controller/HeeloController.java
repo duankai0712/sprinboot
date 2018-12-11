b83d33cf6e5b0aa3e10e053a45f05cb161dfff1c
“your_uduankai <1106734690@qq.com>
2018-12-11 17:49:32 +0800

    mysql 连接

--- src/main/java/com/znsd/springboot/controller/HeeloController.java
+++ src/main/java/com/znsd/springboot/controller/HeeloController.java
@@ -6,6 +6,8 @@ import com.znsd.springboot.entitys.Grede;
 import com.znsd.springboot.entitys.Student;
 import com.znsd.springboot.entitys.StudentService;
 import com.znsd.springboot.error.MyException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -33,7 +37,8 @@ public class HeeloController {
     @Autowired
     private StudentServiceImpl studentServiceImpl;
     @Resource
-    private DataSource dataSource;
+    private DataSource dataSource;     //数据源
+    private Logger logger = LogManager.getLogger(HeeloController.class.getName());     //log4j 日志
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
@@ -58,6 +63,11 @@ public class HeeloController {
         request.setAttribute("grede",list);
         //输出连接池是否连接成功
         System.out.println(this.dataSource.getConnection());
+
+        Date tade = new Date();
+        SimpleDateFormat simp = new SimpleDateFormat();
+        String time = simp.format(tade);
+        logger.error("这是一条错误时间:s"+time);
         return "demo";
     }
 
